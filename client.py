6decd6f6d861ca697ab92d410eda40b240a8bd9e
max.hoch@hotmail.com <max.hoch@hotmail.com>
2016-09-18 16:47:14 +0200

    new feature

--- client.py
+++ client.py
@@ -12,7 +12,11 @@ parser.add_argument("--action", help="available actions: appstats", required=Tru
 parser.add_argument("--appguid", help="app guid")
 parser.add_argument("--appname")
 parser.add_argument("--uaa", help="uaa api")
+parser.add_argument("--warnOnCrashEventSeconds", help="look back x seconds for crash events", type=int)
+parser.add_argument("--disableWarnOnCrash", help="set parameter for disabling")
 _args = parser.parse_args()
+
+
 # api
 GETAPPS = "/v2/apps"
 GETAPPSTATSDETAILS = "/v2/apps/<<appguid>>/stats"
@@ -20,15 +24,22 @@ GETAPPSTATS = "/v2/apps/<<appguid>>/summary"
 GETINFO = "/v2/info"
 CHECKSERVICE = "/check_token"
 GETAPPEVENTS = "/v2/events?q=type:app.crash&q=actee:<<appguid>>&order-direction=desc&q=timestamp><<timestamp>>"
+
+
 BYTE_TO_MB = 1024 * 1024
 SECONDS_TO_MINUTES=60
+if _args.warnOnCrashEventSeconds!=None:
+    SecondsFromLastCrashEvent = int(_args.warnOnCrashEventSeconds)  # 10 min
+else:
+    SecondsFromLastCrashEvent = 600  # 10 min
 
-SecondsFromLastCrashEvent = 600 #10 min
+
 
 
 def getToken(user, password, host, resetToken="no"):
     file = open('.token', 'r')
     token = None
+
     if (file != None and resetToken != "yes"):
         try:
             token = file.read()
@@ -49,37 +60,44 @@ def getToken(user, password, host, resetToken="no"):
         file = open('.token', 'w')
         file.write(token)
         file.close()
+
     return token
 
 
 def getCFHeader(token):
     return {"Authorization": "bearer " + token}
 
+def getParsedURL(url, args):
+    if (args.appguid != None):
+        url = url.replace("<<appguid>>", args.appguid)
+    return url
 
 def getCFData(url, args):
     token = getToken(args.user, args.password, args.api)
     headers = getCFHeader(token)
-    host = args.api
-    if (args.appguid != None):
-        url = url.replace("<<appguid>>", args.appguid)
-    r = requests.get(host + url, headers=headers)
+    url=getParsedURL(url,args)
+
+    r = requests.get(args.api + url, headers=headers)
 
     if r.status_code == 401:
         token = getToken(args.user, args.password, args.api, "yes")
         headers = getCFHeader(token)
-        r = requests.get(host + url, headers=headers)
+        r = requests.get(args.api + url, headers=headers)
 
     return r.content
 
+def getUTCParsedTime(elapsedTime):
+    return str((datetime.datetime.utcnow() - timedelta(seconds=elapsedTime)).isoformat()) + "Z"
 
 def getAppStats(args):
     crashEventsLastMinutes = False
     appSummaryJson = json.loads(getCFData(GETAPPSTATS, args))
-    utcTime = str((datetime.datetime.utcnow() - timedelta(seconds=SecondsFromLastCrashEvent)).isoformat()).replace(" ",
-                                                                                                                   "") + "Z"
-    appEvents = json.loads(getCFData(GETAPPEVENTS.replace("<<timestamp>>", utcTime), args))
-    if len(appEvents["resources"]) > 0:
-        crashEventsLastMinutes = True
+    utcTime=getUTCParsedTime(SecondsFromLastCrashEvent)
+
+    if args.disableWarnOnCrash==None:
+        appEvents = json.loads(getCFData(GETAPPEVENTS.replace("<<timestamp>>", utcTime), args))
+        if len(appEvents["resources"]) > 0:
+            crashEventsLastMinutes = True
 
     perfData = ""
     statusText = "Unknown - not set"
