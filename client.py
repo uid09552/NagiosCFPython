4fbd66ab78dc044930c6f44486463352e3eff49d
max.hoch@hotmail.com <max.hoch@hotmail.com>
2016-09-19 21:49:01 +0200

    refactor

--- client.py
+++ client.py
@@ -19,164 +19,152 @@ _args = parser.parse_args()
 
 
 # api
-GETAPPS = "/v2/apps"
-GETAPPSTATSDETAILS = "/v2/apps/<<appguid>>/stats"
-GETAPPSTATS = "/v2/apps/<<appguid>>/summary"
-GETINFO = "/v2/info"
-CHECKSERVICE = "/check_token"
-GETAPPEVENTS = "/v2/events?q=type:app.crash&q=actee:<<appguid>>&order-direction=desc&q=timestamp><<timestamp>>"
+GET_APPS = "/v2/apps"
+GET_APP_STATS_DETAILS = "/v2/apps/<<appguid>>/stats"
+GET_APP_STATS = "/v2/apps/<<appguid>>/summary"
+GET_INFO = "/v2/info"
+CHECK_SERVICE = "/check_token"
+GET_APP_EVENTS = "/v2/events?q=type:app.crash&q=actee:<<appguid>>&order-direction=desc&q=timestamp><<timestamp>>"
 
 
 BYTE_TO_MB = 1024 * 1024
 SECONDS_TO_HOURS=60*60
-if _args.warnOnCrashEventSeconds!=None:
-    SecondsFromLastCrashEvent = int(_args.warnOnCrashEventSeconds)  # 10 min
+
+
+if _args.warnOnCrashEventSeconds is not None:
+    seconds_from_last_crash_event = int(_args.warnOnCrashEventSeconds)  # 10 min
 else:
-    SecondsFromLastCrashEvent = 600  # 10 min
+    seconds_from_last_crash_event = 600  # 10 min
 
 
-
-
-def getToken(user, password, host,proxy=None, resetToken="no"):
-    file = open('.token', 'r')
+def get_token(user, password, host, proxy=None, reset_token="no"):
+    token_file = open('.token', 'r')
     token = None
 
-    if file is not None and resetToken != "yes":
+    if token_file is not None and reset_token != "yes":
         try:
-            token = file.read()
-            file.close()
+            token = token_file.read()
+            token_file.close()
             if token is not None and token != '':
-                decodedtoken = jwt.decode(token, verify=False)
-                exp = decodedtoken['exp']
+                decoded_token = jwt.decode(token, verify=False)
+                exp = decoded_token['exp']
                 present = int(time.time())
                 if exp < present:
                     token = None
         except Exception, e:
             token = None
 
-    if token is None or resetToken == "yes":
+    if token is None or reset_token == "yes":
         proxyDic = None
         if proxy is not None:
             proxyDic = dict(http=proxy, https=proxy)
         client = CloudFoundryClient(host, proxy=proxyDic)
         client.init_with_user_credentials(user, password)
         token = client.refresh_token
-        tokenfile = open('.token', 'w')
-        tokenfile.write(token)
-        tokenfile.close()
+        token_file = open('.token', 'w')
+        token_file.write(token)
+        token_file.close()
 
     return token
 
 
-def getCFHeader(token):
+def get_cf_header(token):
     return {"Authorization": "bearer " + token}
 
-def getParsedURL(url, args):
+
+def get_parsed_url(url, args):
     if args.appguid is not None:
         url = url.replace("<<appguid>>", args.appguid)
     return url
 
-def getCFData(url, args):
-    token = getToken(args.user, args.password, args.api, args.proxy)
-    headers = getCFHeader(token)
-    url=getParsedURL(url,args)
-    proxyDic=None
+
+def get_cf_data(url, args):
+    token = get_token(args.user, args.password, args.api, args.proxy)
+    headers = get_cf_header(token)
+    url=get_parsed_url(url, args)
+    proxyDic = None
     if args.proxy is not None:
         proxyDic={
             "http": args.proxy,
             "https": args.proxy
         }
-
     r = requests.get(args.api + url, headers=headers, proxies=proxyDic)
-
-
     if r.status_code == 401:
-        token = getToken(args.user, args.password, args.api,args.proxy, "yes")
-        headers = getCFHeader(token)
+        token = get_token(args.user, args.password, args.api, args.proxy, "yes")
+        headers = get_cf_header(token)
         r = requests.get(args.api + url, headers=headers)
-
     return r.content
 
-def getUTCParsedTime(elapsedTime):
-    return str((datetime.datetime.utcnow() - timedelta(seconds=elapsedTime)).isoformat()) + "Z"
 
-def getAppStats(args):
+def get_utc_parsed_time(elapsed_time):
+    return str((datetime.datetime.utcnow() - timedelta(seconds=elapsed_time)).isoformat()) + "Z"
+
+
+def get_app_stats(args):
     crashEventsLastMinutes = False
-    appSummaryJson = json.loads(getCFData(GETAPPSTATS, args))
-    utcTime=getUTCParsedTime(SecondsFromLastCrashEvent)
+    app_summary_json = json.loads(get_cf_data(GET_APP_STATS, args))
+    utc_time = get_utc_parsed_time(seconds_from_last_crash_event)
 
-    if args.disableWarnOnCrash==None:
-        appEvents = json.loads(getCFData(GETAPPEVENTS.replace("<<timestamp>>", utcTime), args))
+    if args.disableWarnOnCrash is None:
+        appEvents = json.loads(get_cf_data(GET_APP_EVENTS.replace("<<timestamp>>", utc_time), args))
         if len(appEvents["resources"]) > 0:
             crashEventsLastMinutes = True
 
-    perfData = ""
-    statusText = "Unknown - not set"
-    state = appSummaryJson["state"]
-    memory = appSummaryJson["memory"]
-    instances = appSummaryJson["instances"]
-    runningInstances = 0
-    disk_quota = appSummaryJson["disk_quota"]
-    maxMemory = 0
-    maxCPU = 0
-    maxDisk = 0
-    minUptime = 9999999999
-    maxUptime = 0
+    perf_data = ""
+    state = app_summary_json["state"]
+    memory = app_summary_json["memory"]
+    instances = app_summary_json["instances"]
+    running_instances = 0
+    disk_quota = app_summary_json["disk_quota"]
+    max_memory = 0
+    max_cpu = 0
+    max_disk = 0
+    min_uptime = sys.maxint
+    max_uptime = 0
 
     if state == "STARTED":
-        appDetailsJson = json.loads(getCFData(GETAPPSTATSDETAILS, args))
-        for appDetail in appDetailsJson:
-            details = appDetailsJson[appDetail]
+        app_details_json = json.loads(get_cf_data(GET_APP_STATS_DETAILS, args))
+        for app_detail in app_details_json:
+            details = app_details_json[app_detail]
             if details["state"] == "RUNNING":
-                uptime = details["stats"]["uptime"]
-                if minUptime > uptime:
-                    minUptime = uptime
-                if maxUptime < uptime:
-                    maxUptime = uptime
-                cpu = int(details["stats"]["usage"]["cpu"] * 100)
-                mem = details["stats"]["usage"]["mem"]
-                disk = details["stats"]["usage"]["disk"]
-                if cpu > maxCPU:
-                    maxCPU = cpu
-                if mem > maxMemory:
-                    maxMemory = mem
-                if disk > maxDisk:
-                    maxDisk = disk
+                min_uptime = min([min_uptime, details["stats"]["uptime"]])
+                max_uptime = max([max_uptime, details["stats"]["uptime"]])
+                max_cpu = max([int(details["stats"]["usage"]["cpu"] * 100), max_cpu])
+                max_memory = max ([int(details["stats"]["usage"]["mem"]), max_memory])
+                max_disk = max([int(details["stats"]["usage"]["disk"]), max_disk])
+                running_instances += 1
 
-                runningInstances += 1
+        if running_instances > 0:
+            perf_data = " | max_memory=" + str(max_memory / (BYTE_TO_MB)) + "MB;" + str(memory * 0.85) + ";" + \
+                       str(memory * 0.9) + ";;;" + "max_cpu=" + str(max_cpu) + ";" + str(95) + ";" + str(100) + ";;;" + \
+                       ", max_disk=" + str(max_disk / (BYTE_TO_MB)) + "MB;" + str(disk_quota * 0.9) + ";" + \
+                       str(disk_quota * 0.9) + ";;;, min_uptime=" + str(min_uptime / SECONDS_TO_HOURS) + ";10;10;;;," \
+                                                                                           " max_uptime=" + str(
+                max_uptime / SECONDS_TO_HOURS) + ";10;10;;;, instances=" + str(running_instances) + ";0;0;;;"
 
-        if runningInstances > 0:
-            perfData = " | maxMemory=" + str(maxMemory / (BYTE_TO_MB)) + "MB;" + str(memory * 0.85) + ";" + \
-                       str(memory * 0.9) + ";;;" + "maxCPU=" + str(maxCPU) + ";" + str(95) + ";" + str(100) + ";;;" + \
-                       ", maxDisk=" + str(maxDisk / (BYTE_TO_MB)) + "MB;" + str(disk_quota * 0.9) + ";" + \
-                       str(disk_quota * 0.9) + ";;;, minUptime=" + str(minUptime / SECONDS_TO_HOURS) + ";10;10;;;," \
-                                                                                           " maxUptime=" + str(
-                maxUptime / SECONDS_TO_HOURS) + ";10;10;;;, instances=" + str(runningInstances) + ";0;0;;;"
-
-    nagiosState = 3  # Unknown
-    if runningInstances == 0 and state=="STARTED":
-        nagiosState = 2  # Critical
-        statusText = "Critical - no running instances"
-    elif runningInstances < instances:
-        nagiosState = 1  # Warning
-        statusText = "Warning - not all instances are running"
-    elif runningInstances == instances:
+    if running_instances == 0 and state=="STARTED":
+        nagios_state = 2  # Critical
+        status_text = "Critical - no running instances"
+    elif running_instances < instances:
+        nagios_state = 1  # Warning
+        status_text = "Warning - not all instances are running"
+    elif running_instances == instances:
         if crashEventsLastMinutes:
-            nagiosState = 0
-            statusText = "Warning - crash events available"
+            nagios_state = 0
+            status_text = "Warning - crash events available"
         else:
-            nagiosState = 0
-            statusText = "OK"
+            nagios_state = 0
+            status_text = "OK"
     else:
-        nagiosState = 3  # Unknown
-        statusText = "Unknown - Unknown state"
-    print (statusText + perfData)
-    sys.exit(nagiosState)
+        nagios_state = 3  # Unknown
+        status_text = "Unknown - Unknown state"
+    print (status_text + perf_data)
+    sys.exit(nagios_state)
 
 
 def main(args):
     if (args.action == "appstats"):
-        getAppStats(args)
+        get_app_stats(args)
 
 
 if __name__ == "__main__":
